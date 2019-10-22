97f6b8812be2eb6dd169a976a03db1e2b8e7bbd7
Rocio <anarocio26@gmail.com>
2019-10-22 11:29:00 -0500

    Added pagination mode to users list

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,15 +3,37 @@ const User = require('../models/user');
 const app = express();
 
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+// This method allows us to get certain amount of users for page
 
 app.get('/user', function (req, res) {
-    res.json('Get Usuario')
+  let pages = req.query.pages || 0;
+    pages = Number(pages)
+  let limit = req.query.limite || 5;
+    limit = Number(limit);
+
+      User.find({})
+          .skip(pages)
+          .limit(limit)
+          .exec((err, usuarios) => {
+            if(err){
+              return res.status(400).json({
+                  ok: false,
+                  err
+              })
+          }
+          res.json({
+            ok: true,
+            usuarios
+          })
+        })
   })
   
 app.post('/user', function (req, res) {
-      let body = req.body;
+      let body = _.pick(req.body, ['name', 'email', 'password', 'image', 'rol', 'state']);
       let user = new User({
-          name: body.nombre,
+          name: body.name,
           email: body.email,
           password: bcrypt.hashSync(body.password, 10),
           image: body.image,
@@ -26,8 +48,6 @@ app.post('/user', function (req, res) {
               })
           }
 
-          // UserDB.password = null;
-
           res.json({
             ok: true,
             user: UserDB
@@ -38,8 +58,18 @@ app.post('/user', function (req, res) {
   
 app.put('/user/:id', function (req, res) {
       let id = req.params.id;
-      res.json({
-          id
+      let body = req.body;
+      User.findByIdAndUpdate(id, body, {new: true, runValidators: true }, (err, UserDB) => {
+        if(err){
+          return res.status(400).json({
+                ok: false,
+                err
+          })
+        }
+        res.json({
+          ok: true,
+          user: UserDB
+          })
       })
   })
   
