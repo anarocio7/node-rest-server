2fadadfb9cf44db6d4b6306b5d0b7ca7f745bd5e
Rocio <anarocio26@gmail.com>
2019-10-28 10:52:41 -0600

    Rest server -user

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,17 @@
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Database environment
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/supercafe'
+} else {
+    urlDB = 'mongodb+srv://anabc56:pyhYxF6EtwCr97uM@cluster0-yqobm.mongodb.net/Users'
+}
+
+process.env.URLDB = urlDB;
