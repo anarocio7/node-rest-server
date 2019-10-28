271e61f863c491625f9711d45679b9c2d242bbbb
Rocio <anarocio26@gmail.com>
2019-10-28 11:20:11 -0600

    Created config variable on mongo connection

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/supercafe'
 } else {
-    urlDB = 'mongodb+srv://anabc56:pyhYxF6EtwCr97uM@cluster0-yqobm.mongodb.net/Users'
+    urlDB = mongoURI
 }
 
 process.env.URLDB = urlDB;
+
---
+++ server/config/mongoConnect.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://anabc56:pyhYxF6EtwCr97uM@cluster0-yqobm.mongodb.net/Users'
+}
